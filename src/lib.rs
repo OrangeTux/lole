fd1757cf8ab2f47a78fa1953f195644abdbfdcef
Auke Oosterhoff <auke@orangetux.nl>
2021-06-16 16:02:33 +0200

    Add `PacketType`
    The `PacketType` represents the different types of packets that
    are defined in F1 2020 UDP API.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,63 @@
 use nom::{
-    bytes::complete::tag,
+    combinator::map_res,
     number::complete::{le_f32, le_u16, le_u32, le_u64, le_u8},
     IResult,
 };
+use std::convert::TryFrom;
+
+/// The F1 2020 API defines 10 different types of packets.
+#[derive(Debug, PartialEq)]
+pub enum PacketType {
+    /// This packet contains physics data for all cars being driven.
+    Motion,
+
+    /// This packet contains details of the session in progress.
+    Session,
+
+    /// The lap data packet gives details of all the cars in the session.
+    LapData,
+
+    /// This packet gives details of events that happen during the course of a session.
+    Event,
+
+    /// This is a list of participants in the race.
+    Participants,
+
+    /// This packet details the car setups for each vehicle in the session.
+    CarSetups,
+
+    /// This packet details telemetry for all the cars in the race
+    CarTelemetry,
+
+    /// This packet details car statuses for all the cars in the race.
+    CarStatus,
+
+    /// This packet details the final classification at the end of the race,
+    FinalClassification,
+
+    /// This packet details the players currently in a multiplayer lobby.
+    LobbyInfo,
+}
+
+impl TryFrom<u8> for PacketType {
+    type Error = &'static str;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Self::Motion),
+            2 => Ok(Self::Session),
+            3 => Ok(Self::LapData),
+            4 => Ok(Self::Event),
+            5 => Ok(Self::Participants),
+            6 => Ok(Self::CarSetups),
+            7 => Ok(Self::CarTelemetry),
+            8 => Ok(Self::CarStatus),
+            9 => Ok(Self::FinalClassification),
+            10 => Ok(Self::LobbyInfo),
+            _ => Err("Invalid value"),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Header {
@@ -10,7 +65,7 @@ pub struct Header {
     pub game_major_version: u8,
     pub game_minor_version: u8,
     pub packet_version: u8,
-    pub packet_id: u8,
+    pub packet_id: PacketType,
     pub session_uid: u64,
     pub session_time: f32,
     pub frame_identifier: u32,
@@ -28,7 +83,7 @@ pub fn header(input: &'static [u8]) -> IResult<&'static [u8], Header> {
     let (input, game_major_version) = le_u8(input)?;
     let (input, game_minor_version) = le_u8(input)?;
     let (input, packet_version) = le_u8(input)?;
-    let (input, packet_id) = le_u8(input)?;
+    let (input, packet_id) = map_res(le_u8, PacketType::try_from)(input)?;
     let (input, session_uid) = le_u64(input)?;
     let (input, session_time) = le_f32(input)?;
     let (input, frame_identifier) = le_u32(input)?;
@@ -61,7 +116,7 @@ fn test_packet_format() {
             game_major_version: 1,
             game_minor_version: 18,
             packet_version: 1,
-            packet_id: 3,
+            packet_id: PacketType::LapData,
             session_uid: 10456883590002387209,
             session_time: 0.0,
             frame_identifier: 0,
